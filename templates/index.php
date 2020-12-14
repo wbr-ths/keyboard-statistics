19a0852b09a907f58bf1ee82f5e894799bb24de1
eliastheis <eliastheis.sg@gmail.com>
2020-12-14 22:46:03 +0100

    Added simple website

---
+++ templates/index.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+	<title>Keyboard Statistics</title>
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="static/css/style.css">
+
+</head>
+
+<body>
+
+	<div class="banner">
+		<h1>Keyboard Statistics</h1>
+	</div>
+	
+	<div class="flex-container">
+
+		<div class="card" id="distribution">
+			<h1>Chart</h1>
+		</div>
+
+		<div class="card" id="test">
+			<h1>Test</h1>
+		</div>
+
+	</div>
+
+</body>
+
+</html>
